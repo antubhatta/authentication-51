2ea64da7d2304d2ec85ed80d97cac828343c2957
antubhatta <antubhatta09@gmail.com>
2023-10-04 01:56:11 +0600

    use context to create user register page

--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -1,14 +1,25 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Register = () => {
-
+    const {createUser}=useContext(AuthContext)
+    
     const handleRegister=e=>{
         e.preventDefault()
         const name=e.target.name.value
         const email= e.target.email.value;
         const password=e.target.password.value
-        
+        // create user in firebase
         console.log(name,email,password)
+        createUser(email,password)
+        .then((result)=>{
+            console.log(result.user)
+
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
     }
     return (
         <div>
